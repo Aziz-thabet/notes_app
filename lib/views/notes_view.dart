1a747d3eba94f9aafc9f8e36947bfdd1d78e964d
Aziz-thabt <azizthabt15@gmail.com>
2023-11-27 15:58:01 +0200

    Add_Note_Button_Sheet.dart

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/Notes_view_body.dart';
 
+import '../widgets/Add_Note_Button_Sheet.dart';
+
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return   Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: (){},
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        showModalBottomSheet(context: context, builder:(context){
+          return const AddNoteButtonSheet();
+        } );
+      },
         child: const Icon(Icons.add),),
         body: const NotesViewBody(),);
   }
